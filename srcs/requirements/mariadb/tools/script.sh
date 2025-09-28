9b8599f56f31e75aef08817fd59bd4ee92f22572
Adrien <chuadrien2003@gmail.com>
2025-09-28 15:47:07 +0200

    FEAT: mariadb +script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,1 +1,13 @@
-echo hello world
+#!/bin/bash
+
+service mysql start
+
+mysql -e "CREATE A DATABASE IF NOT EXISTS ${MYSQL_NAME};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_NAME}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mysql -e "ALTER USER '-u${MYSQL_ROOT_USER}'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u${MYSQL_ROOT_USER} -p${MYSQL_ROOT_PASSWORD} shutdown
+
+exec mysqld_safe
